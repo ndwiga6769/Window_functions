d8fbe5dbfcac60a5e1a754f1e634977b023ffe8e
ndwiga6769 <your.email@example.com>
2024-08-05 16:49:03 +0300

    codes

--- Creating a Running Total Using Window Functions.sql
+++ Creating a Running Total Using Window Functions.sql
@@ -17,3 +17,13 @@ SELECT standard_amt_usd,
        DATE_TRUNC('year', occurred_at) as year,
        SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
 FROM orders
+
+-- Select the id, account_id, and total variable from the orders table, then create a 
+-- column called total_rank that ranks this total amount of paper ordered (from highest to lowest)
+--  for each account using a partition. Your final table should have these four columns
+
+SELECT id,
+       account_id,
+       total,
+       RANK() OVER (PARTITION BY account_id ORDER BY total DESC) AS total_rank
+FROM orders
