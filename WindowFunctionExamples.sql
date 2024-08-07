a2c452885ab15f65fa6cebea94f5f48ee75f08ae
ndwiga6769 <your.email@example.com>
2024-08-07 12:19:42 +0300

    codes

---
+++ WindowFunctionExamples.sql
@@ -0,0 +1,9 @@
+row_number()	bigint	number of the current row within its partition, counting from 1
+rank()			bigint	rank of the current row within its partition, counting from
+dense_rank()	bigint	rank of the current row without gaps; this function counts peer groups
+percent_rank()	double precision	relative rank of the current row: (rank - 1) / (total rows - 1)
+cume_dist()	double precision	relative rank of the current row: (number of rows preceding or peer with current row) / (total rows)
+ntile(num_buckets integer)	integer	integer ranging from 1 to the argument value, dividing the partition as equally as possible
+first_value(value any)	same type as value	returns value evaluated at the row that is the first row of the window frame
+last_value(value any)	same type as value	returns value evaluated at the row that is the last row of the window frame
+nth_value(value any, nth integer)	same type as value	returns value evaluated at the row that is the nth row of the window frame (counting from 1); null if no such row
